65e2971429c51e29840a089485d159cbd02a3a1f
Sean Patrick MacBride <sean.p.macbride@gmail.com>
2026-03-16 02:45:02 -0700

    Add oom improvement to nonuniform shell script

--- galCats/submit/uniformity_03_split.sh
+++ galCats/submit/uniformity_03_split.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --mem=64G
+#SBATCH --mem=128G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=24:00:00
+#SBATCH --time=30:00:00
 #SBATCH -o /global/homes/s/seanmacb/DESC/DESC-GW/gwStreetlights/galCats/out/uniformity_03_%a.out
 #SBATCH -e /global/homes/s/seanmacb/DESC/DESC-GW/gwStreetlights/galCats/err/uniformity_03_%a.err
 #SBATCH --job-name=galcat_uniformity_03
-#SBATCH --array=0-9
+#SBATCH --array=0-8
 #SBATCH --constraint=cpu
 #SBATCH --qos=regular
 #SBATCH --account=m5042
