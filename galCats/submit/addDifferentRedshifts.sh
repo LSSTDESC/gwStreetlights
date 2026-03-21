94407d8221cdef1f930c89d5030a533d02e36c9c
Sean Patrick MacBride <sean.p.macbride@gmail.com>
2026-03-21 11:49:20 -0700

    Adding multiple instances of redshift gen in bash script

--- galCats/submit/addDifferentRedshifts.sh
+++ galCats/submit/addDifferentRedshifts.sh
@@ -2,15 +2,16 @@
 #SBATCH --mem=32G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=20:00:00
-#SBATCH --array=0-8
-#SBATCH -o /global/homes/s/seanmacb/DESC/DESC-GW/gwStreetlights/galCats/out/addDifferentRedshifts_spectro_%A_%a.out
-#SBATCH -e /global/homes/s/seanmacb/DESC/DESC-GW/gwStreetlights/galCats/err/addDifferentRedshifts_spectro_%A_%a.err
-#SBATCH --job-name=addDifferentRedshifts_spectro
+#SBATCH --time=4:00:00
+#SBATCH --array=3
+#SBATCH -o /global/homes/s/seanmacb/DESC/DESC-GW/gwStreetlights/galCats/out/addDifferentRedshifts_modeled_%A_%a.out
+#SBATCH -e /global/homes/s/seanmacb/DESC/DESC-GW/gwStreetlights/galCats/err/addDifferentRedshifts_modeled_%A_%a.err
+#SBATCH --job-name=addDifferentRedshifts_modeled
 #SBATCH --constraint=cpu
 #SBATCH --qos=regular
-#SBATCH --account=m5042
+#SBATCH --account=m1727
 
 source ~/.bashrc
-python /global/homes/s/seanmacb/DESC/DESC-GW/gwStreetlights/galCats/addDifferentRedshifts.py $SLURM_ARRAY_TASK_ID --manifest prodCombinedPaths.txt --input_col redshift_true --output_col redshift_spectro
-# python /global/homes/s/seanmacb/DESC/DESC-GW/gwStreetlights/galCats/addDifferentRedshifts.py $SLURM_ARRAY_TASK_ID --manifest prodCombinedPaths.txt --input_col redshift_measured --output_col redshift_modeled
+# python /global/homes/s/seanmacb/DESC/DESC-GW/gwStreetlights/galCats/addDifferentRedshifts.py $SLURM_ARRAY_TASK_ID --manifest prodCombinedPaths.txt --input_col redshift_true --output_col redshift_spectro
+# sleep 3
+python /global/homes/s/seanmacb/DESC/DESC-GW/gwStreetlights/galCats/addDifferentRedshifts.py $SLURM_ARRAY_TASK_ID --manifest prodCombinedPaths.txt --input_col redshift_true --output_col redshift_modeled
